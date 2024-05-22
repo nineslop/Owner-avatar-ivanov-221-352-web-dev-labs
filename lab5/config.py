1868949662e6aa0089447ffa1267fd58e9059ad5
nineslop <vladislav.ivanov.2004@bk.ru>
2024-05-23 00:53:02 +0300

    Corrections and addition of 5 lab

--- lab5/config.py
+++ lab5/config.py
@@ -2,6 +2,6 @@ SECRET_KEY = "46b74dc7229cda8e0de864283dd32b07ed850f59f97aa1e209fedabf28c81283"
 
 
 MYSQL_USER = "root"
-MYSQL_PASSWORD = "maksimchikson77"
-MYSQL_DATABASE = "db_deynikin"
+MYSQL_PASSWORD = "ktHrF1978"
+MYSQL_DATABASE = "db_ivanov"
 MYSQL_HOST = "localhost"
